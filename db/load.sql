be208dac2be43ed914d5b578b764c61b348732cb
nol3 <nol3@duke.edu>
2021-10-07 11:07:24 -0400

    Update load.sql

--- db/load.sql
+++ db/load.sql
@@ -2,3 +2,4 @@
 \COPY Products FROM 'data/Products.csv' WITH DELIMITER ',' NULL '' CSV
 \COPY Purchases FROM 'data/Purchases.csv' WITH DELIMITER ',' NULL '' CSV
 \COPY Cart FROM 'data/Cart.csv' WITH DELIMITER ',' NULL '' CSV
+\COPY Category FROM 'data/Category.csv' WITH DELIMITER ',' NULL '' CSV 
