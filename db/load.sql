9d2e26475288b55caddf8d3bc6b9ba737d7df3b2
ear53 <edgardy.reyes@duke.edu>
2021-10-07 05:46:16 -0400

    Create test table

--- db/load.sql
+++ db/load.sql
@@ -1,3 +1,4 @@
 \COPY Users FROM 'data/Users.csv' WITH DELIMITER ',' NULL '' CSV
 \COPY Products FROM 'data/Products.csv' WITH DELIMITER ',' NULL '' CSV
 \COPY Purchases FROM 'data/Purchases.csv' WITH DELIMITER ',' NULL '' CSV
+\COPY Cart FROM 'data/Cart.csv' WITH DELIMITER ',' NULL '' CSV
