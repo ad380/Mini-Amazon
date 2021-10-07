abe6121f799b9a00f06ad9651cd5b2bbbeb2f5f8
Ankitha <ad380@duke.edu>
2021-10-06 22:13:05 -0400

    Added transactions table and data

--- db/create.sql
+++ db/create.sql
@@ -10,3 +10,20 @@ CREATE TABLE Users
 CREATE TABLE Sellers
 (seller_id INTEGER NOT NULL PRIMARY KEY REFERENCES Users(uid)
 );
+
+CREATE TABLE Transactions
+(order_id INTEGER NOT NULL PRIMARY KEY,
+ buyer_id INTEGER NOT NULL REFERENCES Users(uid),
+ seller_id INTEGER NOT NULL REFERENCES Sellers(seller_id),
+ date_time TIMESTAMP NOT NULL,
+ product_id INTEGER NOT NULL REFERENCES Products(product_id),
+ quantity INTEGER NOT NULL,
+ total_price INTEGER NOT NULL,
+ status VARCHAR(256)
+ CHECK(CASE
+	WHEN grade = 'f' THEN TRUE
+	WHEN grade = 'nf' THEN TRUE
+	ELSE FALSE
+       END
+ )
+);
