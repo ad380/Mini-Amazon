5445553159c68ca5e603d6bfe1623c68eb6c638f
Ankitha <ad380@duke.edu>
2021-10-28 16:59:18 -0400

    editted python code to get all data from product table

--- app/inventory.py
+++ app/inventory.py
@@ -15,8 +15,7 @@ def index():
     products = Product.get_all(True)
     # find the products current user has bought:
     if current_user.is_authenticated:
-        purchases = Purchase.get_all_by_uid_since(
-            current_user.id, datetime.datetime(1980, 9, 14, 0, 0, 0))
+        purchases = Purchases.get_all(True)
     else:
         purchases = None
     # render the page by adding information to the index.html file
--- app/models/purchase.py
+++ app/models/purchase.py
@@ -32,3 +32,14 @@ ORDER BY time_purchased DESC
                               uid=uid,
                               since=since)
         return [Purchase(*row) for row in rows]
+
+     @staticmethod
+    def get_all_by_seller_id(seller_id):
+        rows = app.db.execute('''
+SELECT id, uid, seller_id, time_purchased, pid, quantity
+FROM Purchases
+WHERE seller_id = :seller_id
+ORDER BY time_purchased DESC
+''',
+                              seller_id=seller_id)
+        return [Purchase(*row) for row in rows]
