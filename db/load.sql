23242485e18cac1a636af38dd07190875d7c7d7d
Tess Noonan <tcn6@duke.edu>
2021-11-02 16:12:59 -0400

    added random gen user data

--- db/load.sql
+++ db/load.sql
@@ -1,4 +1,7 @@
-\COPY Users FROM 'data/Users.csv' WITH DELIMITER ',' NULL '' CSV
+\COPY Users FROM 'Users.csv' WITH DELIMITER ',' NULL '' CSV
+SELECT pg_catalog.setval('public.users_id_seq',
+                         (SELECT MAX(id)+1 FROM Users),
+                         false);
 \COPY Sellers FROM 'data/Sellers.csv' WITH DELIMITER ',' NULL '' CSV
 \COPY Category FROM 'data/Category.csv' WITH DELIMITER ',' NULL '' CSV
 \COPY Products FROM 'data/Products.csv' WITH DELIMITER ',' NULL '' CSV
