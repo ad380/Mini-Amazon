20596476143573b0ec528b0dfd455c30f9586d8c
Kyle Tran <kht11@duke.edu>
2021-11-07 17:02:19 -0500

    Changed the way products are generated so it conforms with all created categories, working on generating purchases

--- db/gen.py
+++ db/gen.py
@@ -1,12 +1,14 @@
 from werkzeug.security import generate_password_hash
 import csv
 from faker import Faker
+import os
+from flask import current_app as app
+
 
 num_users = 100
 num_sellers = 25
 num_products = 2000
 num_purchases = 2500
-categories = ['food', 'clothing', 'gadgets', 'media', 'misc']
 
 Faker.seed(0)
 fake = Faker()
@@ -15,6 +17,16 @@ fake = Faker()
 def get_csv_writer(f):
     return csv.writer(f, dialect='unix')
 
+def get_categories():
+    category_list = []
+    __location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+    f = open(os.path.join(__location__, 'data/Category.csv'))
+    with f as csvfile:
+        category = csv.reader(csvfile)
+        for c in category:
+            category_list.append(c[0])
+    return category_list
 
 def gen_users(num_users):
     with open('Users.csv', 'w') as f:
@@ -48,11 +60,11 @@ def gen_products(num_products):
             seller_id = fake.random_int(min=0, max=num_sellers-1)
             name = fake.sentence(nb_words=4)[:-1]
             description = fake.bs()
+            categories = get_categories()
             category = categories[fake.random_int(min=0, max=len(categories)-1)]
             #TODO: CHANGE IMAGE
             image = "IMAGE"
             price = f'{str(fake.random_int(max=500))}.{fake.random_int(max=99):02}'
-            print(price)
             available = fake.random_element(elements=('true', 'false'))
             if available == 'true':
                 available_pids.append(pid)
@@ -70,26 +82,15 @@ def gen_purchases(num_purchases, available_pids):
             if id % 100 == 0:
                 print(f'{id}', end=' ', flush=True)
             uid = fake.random_int(min=0, max=num_users-1)
-            pid = fake.random_element(elements=available_pids)
             time_purchased = fake.date_time()
+            pid = fake.random_element(elements=available_pids)
+            prod = app.Product()
+            print(prod)
             writer.writerow([id, uid, pid, time_purchased])
+            #[id, uid, seller_id, time_purhased, pid, quantity, total_price, fulfilled]
         print(f'{num_purchases} generated')
     return
 
-def gen_sellers(num_sellers):
-    #First 25 users are sellers
-    with open('Sellers.csv', 'w') as f:
-        writer = get_csv_writer(f)
-        print('Sellers...', end=' ', flush=True)
-        for uid in range(num_sellers):
-            if uid % 5== 0:
-                print(f'{uid}', end=' ', flush=True)
-            writer.writerow([uid])
-        print(f'{num_sellers} generated')
-    return
-
-
 # gen_users(num_users)
-# gen_sellers(num_sellers)
 available_pids = gen_products(num_products)
-# gen_purchases(num_purchases, available_pids)
+gen_purchases(num_purchases, available_pids)
