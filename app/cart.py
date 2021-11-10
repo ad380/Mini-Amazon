83115700c0cb3b3bb53632a6a3f075aa796edd1d
Edgardy Reyes <ear53@duke.edu>
2021-11-10 04:48:43 -0500

    created cart class in flask

---
+++ app/cart.py
@@ -0,0 +1,17 @@
+from flask import render_template, redirect, url_for, flash, request
+from werkzeug.urls import url_parse
+from flask_wtf import FlaskForm
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from flask_babel import _, lazy_gettext as _l
+from .models.user import User
+from .models.product import Product
+from .models.purchase import Purchase
+from flask import Blueprint
+
+bp = Blueprint('cart', __name__)
+
+@bp.route('/cart')
+def cart():
+    if True:
+  #  if current_user.is_authenticated:
+        return render_template('cart.html', title='Cart')
