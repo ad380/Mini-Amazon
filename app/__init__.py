bd1f18f6c2f48fb1606cb6aa07b1948ec39e7126
Kyle Tran <kht11@duke.edu>
2021-12-02 21:39:00 -0500

    Added filter to format dates, update customer review look on product pages

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_login import LoginManager
-from flask_babel import Babel
+from flask_babel import Babel, format_datetime
 from .config import Config
 from .db import DB
 
@@ -33,4 +33,20 @@ def create_app():
     from .cart import bp as cart_bp
     app.register_blueprint(cart_bp)
 
+    @app.template_filter()
+    def format_date(value, format='medium'):
+        """
+        Custom filter to help format dates in HTML
+        """
+        if format == 'full':
+            format="EEEE, d. MMMM y 'at' HH:mm"
+        elif format == 'medium':
+            format="EE dd.MM.y HH:mm"
+        elif format == 'standard':
+            format="MMMM d, y"
+        return format_datetime(value, format)
+
     return app
+
+
+    
