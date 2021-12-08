e2a901ea82cbbb8cd9aaa39d4d9862633640ca84
nol3 <nol3@duke.edu>
2021-12-07 21:13:25 -0500

    Update app/products.py

--- app/products.py
+++ app/products.py
@@ -43,8 +43,6 @@ def products(pid, sortoption=0):
     review_keys = [(r.product_id, r.buyer_id) for r in reviews]
     review_upvotes = [ProductReview.get_upvotes(pid, bid) for pid, bid in review_keys]
 
-    user_votes = [ProductReview.get_votes_from(current_user.id, pid, bid) for bid in reviewer_ids]
-
     top_reviews = ProductReview.get_top_reviews(pid)
     top_reviewer_ids = [r.buyer_id for r in top_reviews]
     top_reviewer_names = [User.get_name(id) for id in top_reviewer_ids]
